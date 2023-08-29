b48ea7de05bb0281302038c63a1c82dea2822194
rfujioka <rodrigofujioka@gmail.com>
2023-08-29 18:35:06 -0300

    Preparação do projeto para 2023.2 IESP

--- src/main/java/br/com/fujideia/iesp/tecback/controller/UsuarioController.java
+++ src/main/java/br/com/fujideia/iesp/tecback/controller/UsuarioController.java
@@ -1,53 +1,37 @@
 package br.com.fujideia.iesp.tecback.controller;
 
+/* - QUARTO PASSO
+Primeiro vamos mapear os requeste map,
+*  -metodo quer salva Post */
+
+import br.com.fujideia.iesp.tecback.model.Filme;
 import br.com.fujideia.iesp.tecback.model.Usuario;
-import br.com.fujideia.iesp.tecback.model.dto.UsuarioDTO;
-import br.com.fujideia.iesp.tecback.model.dto.UsuarioNomeEmailDTO;
 import br.com.fujideia.iesp.tecback.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping ("/Usuario")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
 
-    @PostMapping
-    public ResponseEntity<UsuarioDTO> salvar(@RequestBody @Validated  UsuarioDTO usuarioDTO){
-        usuarioDTO = service.salvar(usuarioDTO);
-        return ResponseEntity.ok(usuarioDTO);
-    }
 
-    @PutMapping
-    public ResponseEntity<Usuario> alterar(@RequestBody Usuario usuario){
-        usuario = service.alterar(usuario);
+    @PostMapping
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+        usuario = service.salvar(usuario);
         return ResponseEntity.ok(usuario);
     }
 
     @GetMapping
-    public ResponseEntity<List<UsuarioNomeEmailDTO>> listar(){
+    public ResponseEntity<List<Usuario>> listar(){
         return ResponseEntity.ok(service.listar());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> consultar(@PathVariable("id") Integer id){
-        return ResponseEntity.ok(service.consultarPorId(id));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> excluir(@PathVariable("id") Integer id){
-        if(service.excluir(id)){
-            return ResponseEntity.ok().build();
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-    }
 
 
 }
--- src/main/java/br/com/fujideia/iesp/tecback/repository/UsuarioRepository.java
+++ src/main/java/br/com/fujideia/iesp/tecback/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package br.com.fujideia.iesp.tecback.repository;
 
-
 import br.com.fujideia.iesp.tecback.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*  -SEGUNDO PASSO
+no Repository manipular o banco  como add exluir etc..*/
+
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+public interface UsuarioRepository extends JpaRepository <Usuario, Integer > {
+
 }
